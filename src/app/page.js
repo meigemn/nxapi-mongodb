a06f289359b1b2cbbdf71b60884434d919cfd4a3
meigemn <carloscc9297@gmail.com>
2025-02-13 21:49:33 +0100

    añadida linea de cors

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,13 @@
 import { redirect } from "next/navigation"
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
+app.use(cors({origin: 'https://nxapi-mongodbcarlos.vercel.app/api/products'}))
+
+app.get('/', (req, res) => {
+  res.redirect('/api/products')
+})
 export default function Home() {
   redirect('/api/products')
 }
